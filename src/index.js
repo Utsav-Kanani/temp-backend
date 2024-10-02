371242c398714f0b1e8aa74816a732c1f4b24650
Utsav Kanani <utsavkanani2004@gmail.com>
2024-10-02 15:39:53 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,45 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'    
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express()
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MOGODB_URI}/${DB_NAME}`)
+        app.on("error", () => {
+            console.log("ERR: ",error);
+            throw error
+        }) 
+
+        app.listen(process.env.PORT , (error)=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    }
+    catch(error){
+        console.error("ERROR: ",error)
+        throw err
+    }
+})
+
+*/
